e7e5e4cdd7002a52c7521eeeaec65b4bad8e6555
Nhi Trinh Yen <trinhyennhi0705@gmail.com>
2025-03-26 23:43:28 +0700

    Add files via upload

---
+++ Aims.java
@@ -0,0 +1,22 @@
+public class Aims {
+	public static void main(String[] args) {
+		// Create a new cart
+		Cart anOrder = new Cart();
+
+		// Create new DVD objects and add them to the cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+
+		anOrder.addDigitalVideoDisc(dvd1);
+
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fictions", "George Lucas", 87, 24.95f);
+
+		anOrder.addDigitalVideoDisc(dvd2);
+
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+		anOrder.addDigitalVideoDisc(dvd3);
+
+		System.out.println("Total cost is: ");
+		System.out.println(anOrder.totalCost());
+	}
+}
